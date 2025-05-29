f76dff913a155d6583086220e1dcb85fdf12371f
SheikhSalman <assalmanmuhammad@gmail.com>
2025-05-29 16:49:55 +0600

    find one data

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const app = express()
-const port = process.env.PORT || 300
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 3000
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -12,7 +12,6 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dclhmji.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -24,10 +23,25 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const jobCollection = client.db('career-code').collection('jobs')
+
+    app.get('/jobs', async(req, res)=>{
+        const result = await jobCollection.find().toArray()
+        res.send(result)
+    })
+
+    app.get('/jobs/:id', async(req,res)=>{
+        const id = req.params.id
+        const query = {_id : new ObjectId(id)}
+        const result = await jobCollection.findOne(query)
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
