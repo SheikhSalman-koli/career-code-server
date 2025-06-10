db885faa0970e3ad73d5a5c3d8396bb23991019d
SheikhSalman <assalmanmuhammad@gmail.com>
2025-06-10 10:05:41 +0600

    jwt

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
+const cookieparser = require('cookie-parser')
 const app = express()
 const port = process.env.PORT || 3000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-app.use(cors())
+// middle ware
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials:true
+}))
 app.use(express.json())
+app.use(cookieparser())
 
+const logger = (req, res, next) =>{
+    console.log('inside the logger middleware');
+    next()
+}
 
+const verifyToken = (req, res, next) =>{
+  const token = req?.cookies?.token
+  console.log('cookies in the middleware', token);
+
+  if(!token){
+    return res.status(401).send({message: 'unauthorized access'})
+  }
+  // verify token
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded)=>{
+    if(err){
+      return res.status(401).send({message: 'unauthorized access'})
+    }
+    req.decoded = decoded
+   
+    next()
+  })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dclhmji.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -28,10 +56,23 @@ async function run() {
     const jobCollection = client.db('career-code').collection('jobs')
     const jobApplication = client.db('career-code').collection('applications')
 
+    // jwt token api
+   app.post('/jwt', (req,res)=>{
+      const userData = req.body
+      const token = jwt.sign(userData, process.env.JWT_SECRET, {expiresIn: '1d'})
+
+      // set the token in cookie 
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: false
+      })
+      res.send({success: true})
+   })
+
     app.get('/jobs', async (req, res) => {
       const email = req.query.email
       const query = {}
-      if(email){
+      if (email) {
         query.hr_email = email
       }
       // const result = await jobCollection.find(query).toArray()
@@ -48,6 +89,21 @@ async function run() {
     //    res.send(result)
     // })
 
+
+    app.get('/jobs/applications', async(req, res)=>{
+       const email = req.query.email
+       const query = {hr_email: email}
+       const jobs = await jobCollection.find(query).toArray()
+
+       for(const job of jobs){
+        const applicationQuery = {jobId: job._id.toString()}
+        const application_count = await jobApplication.countDocuments(applicationQuery)
+        job.application_count = application_count
+       }
+
+       res.send(jobs)
+    })
+
     app.get('/jobs/:id', async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
@@ -55,24 +111,28 @@ async function run() {
       res.send(result)
     })
 
-    app.post('/jobs', async(req, res)=>{
-       const newjob = req.body
-       const result = await jobCollection.insertOne(newjob)
-       res.send(result)
+    app.post('/jobs', async (req, res) => {
+      const newjob = req.body
+      const result = await jobCollection.insertOne(newjob)
+      res.send(result)
     })
 
-    app.get('/applications', async (req, res) => {
+    app.get('/applications',logger,verifyToken, async (req, res) => {
       const email = req.query.email
+      // console.log('cookies',req.cookies);
+      if(email !== req.decoded.email){
+         return res.status(403).send({message: 'forbidden access'})
+         
+      }
       const query = {
-        applicant : email
+        applicant: email
       }
       const result = await jobApplication.find(query).toArray()
-
       // connect a collection with anouther collection
       // bad way
-      for(const application of result){
+      for (const application of result) {
         const id = application.jobId
-        const jobQuery = {_id : new ObjectId(id)}
+        const jobQuery = { _id: new ObjectId(id) }
         const job = await jobCollection.findOne(jobQuery)
         application.company = job.company
         application.title = job.title
@@ -80,6 +140,26 @@ async function run() {
       }
 
       res.send(result)
+    })
+
+    app.get('/applications/job/:job_id', async (req, res) => {
+      const job_id = req.params.job_id
+      const query = { jobId: job_id }
+      const result = await jobApplication.find(query).toArray()
+      res.send(result)
+    })
+
+    app.patch('/applications/:id', async(req, res)=>{
+       const id = req.params.id
+       const filter = { _id: new ObjectId(id) }
+       const updatedDoc = {
+         $set: {
+           status: req.body.status
+         }
+       }
+       const result = await jobApplication.updateOne(filter, updatedDoc)
+       res.send(result)
+
     })
 
     app.post('/applications', async (req, res) => {
